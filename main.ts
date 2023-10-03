70160442431c4d1f88619b472eb57a3f14856913
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-10-02 20:23:54 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const ast = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     ship.y += -5
 })
@@ -6,10 +9,14 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     blast.setFlag(SpriteFlag.DestroyOnWall, true)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    changeSpeed(0.9)
+    changeSpeed(0.75)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.ast, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+    info.changeScoreBy(randint(10, 30))
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    changeSpeed(1.1)
+    changeSpeed(1.2)
 })
 function changeSpeed (delta: number) {
     speed = speed * delta
@@ -19,9 +26,16 @@ function changeSpeed (delta: number) {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     ship.y += 5
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.ast, function (sprite, otherSprite) {
+    music.play(music.melodyPlayable(music.knock), music.PlaybackMode.UntilDone)
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+})
+let asteroid: Sprite = null
 let blast: Sprite = null
 let ship: Sprite = null
 let speed = 0
+info.setLife(5)
 scene.setBackgroundColor(15)
 speed = 1
 let mscale = 0
@@ -32,3 +46,19 @@ ship = sprites.create(assets.image`Xwing`, SpriteKind.Player)
 ship.setPosition(18, 55)
 ship.changeScale(0.75, ScaleAnchor.Middle)
 ship.setStayInScreen(true)
+let rocks = [
+assets.image`rock`,
+assets.image`rock0`,
+assets.image`rock1`,
+assets.image`rock2`
+]
+forever(function () {
+    pause(200 * randint(3, 9))
+    asteroid = sprites.create(rocks._pickRandom(), SpriteKind.ast)
+    asteroid.setBounceOnWall(true)
+    asteroid.setFlag(SpriteFlag.AutoDestroy, true)
+    asteroid.setPosition(153, randint(5, 100))
+    asteroid.setVelocity(randint(10, 90), randint(10, 90))
+    pause(2000)
+    sprites.destroy(asteroid)
+})
